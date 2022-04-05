dcf591e485384b44dd316f33e3c21d39b91d97e2
brccabral <cabral.brc@gmail.com>
2022-04-04 20:10:12 -0700

    convert km to miles

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ while True:
     if event == "-CONVERT-":
         input_value: str = values["-INPUT-"]
         if input_value.isnumeric():
-            print(input_value)
+            if values["-UNITS-"] == "km to mile":
+                output = round(float(input_value) * 0.6214, 2)
+                output_string = f"{input_value} km are {output} miles."
+
+            window["-OUTPUT-"].update(output_string)
 
 window.close()
