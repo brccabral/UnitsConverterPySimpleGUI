f76d15515d487457df60faf2c2ededdc0ea8fbb8
brccabral <cabral.brc@gmail.com>
2022-04-04 19:35:47 -0700

    print values from items that return values

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 layout = [
     [sg.Text("Text", enable_events=True, key="-TEXT-"), sg.Spin(["item 1", "item 2"])],
     [sg.Button("Button", key="-BUTTON1-")],
-    [sg.Input()],
+    [sg.Input(key='-INPUT-')],
     [sg.Text("Test"), sg.Button("Button", key="-BUTTON2-")],
 ]
 
@@ -17,7 +17,8 @@ while True:
         break
 
     if event == "-BUTTON1-":
-        print("Button pressed")
+        print(values)
+        print(values['-INPUT-'])
     elif event == "-BUTTON2-":
         print("Test Button pressed")
     elif event == "-TEXT-":
