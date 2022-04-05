3db567d361ecffed4660140604552ebc91cdb61e
brccabral <cabral.brc@gmail.com>
2022-04-04 20:07:20 -0700

    converter starter code

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import PySimpleGUI as sg
 
 
 layout = [
-    [sg.Text("Text", enable_events=True, key="-TEXT1-"), sg.Spin(["item 1", "item 2"])],
-    [sg.Button("Button", key="-BUTTON1-")],
-    [sg.Input(key="-INPUT-")],
-    [sg.Text("Test", key="-TEXT2-"), sg.Button("Button", key="-BUTTON2-")],
+    [
+        sg.Input(key="-INPUT-"),
+        sg.Spin(["km to mile", "kg to pound", "sec to min"], key="-UNITS-"),
+        sg.Button("Convert", key="-CONVERT-"),
+    ],
+    [sg.Text("Output", key="-OUTPUT-")],
 ]
 
 window = sg.Window(title="Converter", layout=layout)
@@ -16,14 +18,9 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
-    if event == "-BUTTON1-":
-        print(values)
-        print(values["-INPUT-"])
-        window["-TEXT1-"].update(values["-INPUT-"])
-        window["-TEXT2-"].update(visible=False)
-    elif event == "-BUTTON2-":
-        print("Test Button pressed")
-    elif event == "-TEXT-":
-        print("text was pressed")
+    if event == "-CONVERT-":
+        input_value: str = values["-INPUT-"]
+        if input_value.isnumeric():
+            print(input_value)
 
 window.close()
