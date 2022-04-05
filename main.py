c69f0bb38e32e1488493aa48bfee9de2d588940a
brccabral <cabral.brc@gmail.com>
2022-04-04 18:45:45 -0700

    added infinity loop

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import PySimpleGUI as sg
 
 
 layout = [
-    [sg.Text('Text')],
-    [sg.Button('Button')],
-    [sg.Input()]
+    [sg.Text("Text"), sg.Spin(["item 1", "item 2"])],
+    [sg.Button("Button")],
+    [sg.Input()],
+    [sg.Text("Test"), sg.Button("Test Button")],
 ]
 
-sg.Window(title='Converter', layout=layout).read()
+window = sg.Window(title="Converter", layout=layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
